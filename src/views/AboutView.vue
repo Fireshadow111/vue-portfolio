a3a60463214680993f1d7d2988df299330e04731
Fireshadow111 <muhammadrajah16@gmail.com>
2024-01-09 15:00:47 +0200

    adding aboutPage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,28 +5,36 @@
         <div class="container text-center">
       <div class="row align-items-center">
 
-        <div class = "about-head-div">
-          <h1 class = "about-me-head">About me</h1>
+        <div id = "about-head-div" class = "mt-2">
+          <h1 id = "about-me-head">About me</h1>
     
         </div>
      
-        <div class="col-md-8 col-sm-8">
+        <div class="col-md-8 col-sm-8 mt-4">
 
-          <h2 class = "about-me-title">Who am I?</h2>
+          <div id = info-con>
+          <h2 id = "about-me-title" class = "mt-">Who am I?</h2>
 
-          <h5 class = "about-me-body">I am an ambitious individual who loves to encounter new problems and I take pleasure in solving them. I have a great passion for expressing myself through my work. I believe that life is a never-ending journey of learning, there is always something new to learn, whether it is a new skill, a new piece of information, or a new way of looking at things. I am always willing to learn and not afraid to ask questions.</h5>
-
-      <h2 class = "about-me-pi">Personal Details</h2>
-        
-      <h5 class = "about-me-pi-bod">Date of birth: 11 April 2001</h5> 
-          <h5 class = "about-me-pi-bod">Location: Heideveld </h5>
-          <h5 class = "about-me-pi-bod">Cell No: 067 774 1883 </h5>
-          <h5 class = "about-me-pi-bod">Email: muhammadrajah22@gmail.com</h5>
+          <h5 id = "about-me-body" class = "mb-3 mx-3">I am an ambitious individual who loves to encounter new problems and I take pleasure in solving them. I have a great passion for expressing myself through my work. I believe that life is a never-ending journey of learning, there is always something new to learn, whether it is a new skill, a new piece of information, or a new way of looking at things. I am always willing to learn and not afraid to ask questions.</h5>
 
         </div>
+        
+      <div id = "about-pi-con" class = "mt-5">
+
+        <h2 id = "about-me-pi">Personal Details</h2>
+
+      <h5 id = "about-me-pi-bod1">Date of birth: 11 April 2001</h5> 
+          <h5 id = "about-me-pi-bod2">Location: Heideveld </h5>
+          <h5 id = "about-me-pi-bod3">Cell No: 067 774 1883 </h5>
+          <h5 id = "about-me-pi-bod4">Email: muhammadrajah22@gmail.com</h5>
+        </div>
+       
+        
+      </div>
+   
         <div class="col-lg-4 col-sm-4">
-          <div class = "about-img-div">
-            <img class = "about-img" src="https://i.postimg.cc/kg5pvk75/image-1-removebg-preview.png" alt="">
+          <div id = "about-img-div">
+            <img id = "about-img" class = "mt-4" src="https://cdn-images.imagevenue.com/67/45/01/ME17G5I8_o.png" alt="">
         </div>
       </div>
       </div>
@@ -47,5 +55,61 @@ export default {
   color: #44d62c;
 }
 
+#about-img{
+  border-radius: 50px;
+  border: 3px solid #44d62c;
+}
+
+#about-me-head{
+  color: white;
+  border: 4px solid #44d62c ;
+  border-radius: 50px;
+  font-family: "DM Serif Display";
+  font-size: 60px;
+
+}
+
+#about-me-title{
+  color: white;
+  border-radius: 20px;
+  font-size: 41px;
+  text-decoration: underline;
+  text-decoration-color: #44d62c;
+}
+
+#info-con{
+  border: 2px solid #44d62c ;
+  border-radius: 50px;
+}
+
     
+#about-pi-con{
+  border: 2px solid #44d62c ;
+  border-radius: 50px;
+}
+
+#about-me-pi{
+  color: white;
+  border-radius: 20px;
+  font-size: 41px;
+  text-decoration: underline;
+  text-decoration-color: #44d62c;
+}
+
+/* #about-me-pi-bod1{
+
+}
+
+#about-me-pi-bod2{
+  
+}
+
+#about-me-pi-bod3{
+  
+}
+
+#about-me-pi-bod4{
+  
+} */
+
 </style>
