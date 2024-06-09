6dd653e99add166586d09c22a8ee080f55404cc9
Fireshadow111 <muhammadrajah16@gmail.com>
2024-06-09 19:37:37 +0200

    adding fav help

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = defineConfig(
+  {
+      transpileDependencies: true,
+      // chainWebpack: config => {
+      //     config
+      //     .plugin('html')
+      //     .tap(args => {
+      //         console.log(args)
+      //         args[0].title = "Capstone Bud"
+      //         return args
+      //     })
+      // },
+      // pwa: {
+      //     iconPaths: {
+      //         favicon32: "someicon.ico",
+      //         favicon16: "someicon.ico"
+      //     }
+      // }
+  }
+);
