f47597c06cb02e28ed602a9152e7c2422adffef1
Fireshadow111 <muhammadrajah16@gmail.com>
2024-06-19 21:01:18 +0200

    adding api and fav

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = defineConfig(
-  {
-      transpileDependencies: true,
-      // chainWebpack: config => {
-      //     config
-      //     .plugin('html')
-      //     .tap(args => {
-      //         console.log(args)
-      //         args[0].title = "Capstone Bud"
-      //         return args
-      //     })
-      // },
-      // pwa: {
-      //     iconPaths: {
-      //         favicon32: "someicon.ico",
-      //         favicon16: "someicon.ico"
-      //     }
-      // }
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+      config
+          .plugin('html')
+          .tap(args => {
+            args[0].title = "Portfolio"; 
+            console.log(args);
+            return args;
+          });
+  },
+  pwa: {
+      iconPaths: {
+          favicon32: "img/icons/favicon.ico", 
+          favicon16: "img/icons/favicon.ico"  
+      }
   }
-);
+});
